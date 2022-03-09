a859fbcc06af8573b5d23cf9f65ffca259017e30
Jonas Herrmannsdörfer <jonas@herrmannsdoerfer.dev>
2022-03-09 20:17:18 +0100

    notifications added

--- components/Elements/Table/Table.stories.tsx
+++ components/Elements/Table/Table.stories.tsx
@@ -1,9 +1,9 @@
-import { Meta, Story } from '@storybook/react';
+import { Meta, Story } from "@storybook/react";
 
-import { Table, TableProps } from './Table';
+import { Table, TableProps } from "./Table";
 
 const meta: Meta = {
-  title: 'Components/Elements/Table',
+  title: "Components/Elements/Table",
   component: Table,
   parameters: {
     controls: { expanded: true },
@@ -22,42 +22,44 @@ type User = {
 
 const data: User[] = [
   {
-    id: '1',
-    name: 'Jane Cooper',
-    title: 'Regional Paradigm Technician',
-    role: 'Admin',
-    email: 'jane.cooper@example.com',
+    id: "1",
+    name: "Jane Cooper",
+    title: "Regional Paradigm Technician",
+    role: "Admin",
+    email: "jane.cooper@example.com",
   },
   {
-    id: '2',
-    name: 'Cody Fisher',
-    title: 'Product Directives Officer',
-    role: 'Owner',
-    email: 'cody.fisher@example.com',
+    id: "2",
+    name: "Cody Fisher",
+    title: "Product Directives Officer",
+    role: "Owner",
+    email: "cody.fisher@example.com",
   },
 ];
 
-const Template: Story<TableProps<User>> = (props) => <Table<User> {...props} />;
+const Template: Story<TableProps<User>> = (props: any) => (
+  <Table<User> {...props} />
+);
 
 export const Default = Template.bind({});
 Default.args = {
   data,
   columns: [
     {
-      title: 'Name',
-      field: 'name',
+      title: "Name",
+      field: "name",
     },
     {
-      title: 'Title',
-      field: 'title',
+      title: "Title",
+      field: "title",
     },
     {
-      title: 'Role',
-      field: 'role',
+      title: "Role",
+      field: "role",
     },
     {
-      title: 'Email',
-      field: 'email',
+      title: "Email",
+      field: "email",
     },
   ],
 };
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,11 +4,13 @@ import { AuthProvider } from "@/lib/auth";
 import { QueryClientProvider } from "react-query";
 import { queryClient } from "@/lib/react-query";
 import { ReactQueryDevtools } from "react-query/devtools";
+import { Notifications } from "@/components/Notifications";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <QueryClientProvider client={queryClient}>
       {process.env.NODE_ENV !== "test" && <ReactQueryDevtools />}
+      <Notifications />
       <AuthProvider>
         <Component {...pageProps} />
       </AuthProvider>
